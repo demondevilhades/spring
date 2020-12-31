a50be1039b7e9bd1f2048c42adeb619c28729789
demondevilhades <demonhades@foxmail.com>
2020-12-31 11:14:58 +0800

    log name config

--- springboot-docker/src/main/java/test/App.java
+++ springboot-docker/src/main/java/test/App.java
@@ -1,5 +1,8 @@
 package test;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -13,8 +16,13 @@ public class App {
     /**
      * 
      * @param args
+     * @throws UnknownHostException 
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws UnknownHostException {
+        InetAddress localHost = InetAddress.getLocalHost();
+        System.setProperty("net.hostName", localHost.getHostName());
+        System.setProperty("net.canonicalHostName", localHost.getCanonicalHostName());
+        System.setProperty("net.hostAddress", localHost.getHostAddress());
         SpringApplication.run(App.class, args);
     }
 }
